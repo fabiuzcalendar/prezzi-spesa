165b94cd4c68531311a44b74d09673be8a497317
fabiuzcalendar <santolinfabio7@gmail.com>
2026-08-14 13:59:59 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "prezzi-spesa-v4";
+const CACHE_NAME = "prezzi-spesa-v5";
 
 const FILES_TO_CACHE = [
   "./",
@@ -9,11 +9,9 @@ const FILES_TO_CACHE = [
   "./manifest.json"
 ];
 
-
 self.addEventListener(
   "install",
   event => {
-
     event.waitUntil(
       caches
         .open(CACHE_NAME)
@@ -23,23 +21,17 @@ self.addEventListener(
     );
 
     self.skipWaiting();
-
   }
 );
-
 
 self.addEventListener(
   "activate",
   event => {
-
     event.waitUntil(
-
       caches
         .keys()
         .then(keys => {
-
           return Promise.all(
-
             keys
               .filter(
                 key =>
@@ -49,61 +41,45 @@ self.addEventListener(
                 key =>
                   caches.delete(key)
               )
-
           );
-
         })
-
     );
 
     self.clients.claim();
-
   }
 );
-
 
 self.addEventListener(
   "fetch",
   event => {
-
     if (
       event.request.method !== "GET"
     ) {
       return;
     }
 
-
     event.respondWith(
-
       fetch(event.request)
-
         .then(response => {
-
           const copia =
             response.clone();
 
           caches
             .open(CACHE_NAME)
             .then(cache => {
-
               cache.put(
                 event.request,
                 copia
               );
-
             });
 
           return response;
-
         })
-
         .catch(() =>
           caches.match(
             event.request
           )
         )
-
     );
-
   }
 );
