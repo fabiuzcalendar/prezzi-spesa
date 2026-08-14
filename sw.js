70299142198a26ca010d25c05da76f0ccba453a8
fabiuzcalendar <santolinfabio7@gmail.com>
2026-08-14 15:50:05 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "prezzi-spesa-v9";
+const CACHE_NAME = "prezzi-spesa-v10-finale";
 
 const FILES_TO_CACHE = [
   "./",
@@ -9,77 +9,60 @@ const FILES_TO_CACHE = [
   "./manifest.json"
 ];
 
-self.addEventListener(
-  "install",
-  event => {
-    event.waitUntil(
-      caches
-        .open(CACHE_NAME)
-        .then(cache =>
-          cache.addAll(FILES_TO_CACHE)
-        )
-    );
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
 
-    self.skipWaiting();
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") {
+    return;
   }
-);
 
-self.addEventListener(
-  "activate",
-  event => {
-    event.waitUntil(
-      caches
-        .keys()
-        .then(keys => {
-          return Promise.all(
-            keys
-              .filter(
-                key =>
-                  key !== CACHE_NAME
-              )
-              .map(
-                key =>
-                  caches.delete(key)
-              )
-          );
-        })
-    );
-
-    self.clients.claim();
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) {
+    return;
   }
-);
 
-self.addEventListener(
-  "fetch",
-  event => {
-    if (
-      event.request.method !== "GET"
-    ) {
-      return;
-    }
-
+  if (event.request.mode === "navigate") {
     event.respondWith(
       fetch(event.request)
         .then(response => {
-          const copia =
-            response.clone();
-
-          caches
-            .open(CACHE_NAME)
-            .then(cache => {
-              cache.put(
-                event.request,
-                copia
-              );
-            });
-
+          const copia = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put("./index.html", copia));
           return response;
         })
-        .catch(() =>
-          caches.match(
-            event.request
-          )
+        .catch(async () =>
+          (await caches.match("./index.html")) || (await caches.match("./"))
         )
     );
+    return;
   }
-);
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        if (response && response.ok) {
+          const copia = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copia));
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
