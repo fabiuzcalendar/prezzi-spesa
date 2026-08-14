73c5ca15e01d992a5a87ce62245d0d2c029fd38b
fabiuzcalendar <santolinfabio7@gmail.com>
2026-08-14 15:46:51 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,80 +1,175 @@
 const DB_NAME = "PrezziSpesaDB";
 const DB_VERSION = 1;
 
+const ARCHIVI = [
+  "puntiVendita",
+  "prodotti",
+  "rilevazioniPrezzi",
+  "listaSpesa"
+];
+
+let dbPromise = null;
+
 function openDatabase() {
-  return new Promise((resolve, reject) => {
+  if (dbPromise) {
+    return dbPromise;
+  }
+
+  dbPromise = new Promise((resolve, reject) => {
     const request = indexedDB.open(DB_NAME, DB_VERSION);
 
-    request.onupgradeneeded = function (event) {
+    request.onupgradeneeded = event => {
       const db = event.target.result;
 
-      if (!db.objectStoreNames.contains("puntiVendita")) {
-        db.createObjectStore("puntiVendita", {
-          keyPath: "id",
-          autoIncrement: true
-        });
-      }
-
-      if (!db.objectStoreNames.contains("prodotti")) {
-        db.createObjectStore("prodotti", {
-          keyPath: "id",
-          autoIncrement: true
-        });
-      }
-
-      if (!db.objectStoreNames.contains("rilevazioniPrezzi")) {
-        db.createObjectStore("rilevazioniPrezzi", {
-          keyPath: "id",
-          autoIncrement: true
-        });
-      }
-
-      if (!db.objectStoreNames.contains("listaSpesa")) {
-        db.createObjectStore("listaSpesa", {
-          keyPath: "id",
-          autoIncrement: true
-        });
-      }
+      ARCHIVI.forEach(nomeArchivio => {
+        if (!db.objectStoreNames.contains(nomeArchivio)) {
+          db.createObjectStore(nomeArchivio, {
+            keyPath: "id",
+            autoIncrement: true
+          });
+        }
+      });
     };
 
-    request.onsuccess = function () {
-      resolve(request.result);
+    request.onsuccess = () => {
+      const db = request.result;
+
+      db.onversionchange = () => {
+        db.close();
+        dbPromise = null;
+      };
+
+      resolve(db);
     };
 
-    request.onerror = function () {
+    request.onerror = () => {
+      dbPromise = null;
       reject(request.error);
     };
+
+    request.onblocked = () => {
+      console.warn("Apertura database bloccata da un'altra scheda.");
+    };
   });
+
+  return dbPromise;
 }
 
-async function aggiungiDato(archivio, dato) {
+async function eseguiRichiesta(archivio, modalita, operazione) {
   const db = await openDatabase();
 
   return new Promise((resolve, reject) => {
-    const transaction = db.transaction(archivio, "readwrite");
+    const transaction = db.transaction(archivio, modalita);
     const store = transaction.objectStore(archivio);
-    const request = store.add(dato);
+    let request;
+    let risultato;
 
-    request.onsuccess = () => resolve(request.result);
+    try {
+      request = operazione(store);
+    } catch (error) {
+      reject(error);
+      return;
+    }
+
+    request.onsuccess = () => {
+      risultato = request.result;
+    };
+
     request.onerror = () => reject(request.error);
+    transaction.oncomplete = () => resolve(risultato);
+    transaction.onerror = () => reject(transaction.error);
+    transaction.onabort = () => reject(transaction.error || new Error("Operazione annullata"));
   });
 }
 
-async function leggiTutti(archivio) {
+function aggiungiDato(archivio, dato) {
+  return eseguiRichiesta(
+    archivio,
+    "readwrite",
+    store => store.add(dato)
+  );
+}
+
+function salvaDato(archivio, dato) {
+  return eseguiRichiesta(
+    archivio,
+    "readwrite",
+    store => store.put(dato)
+  );
+}
+
+function leggiDato(archivio, id) {
+  return eseguiRichiesta(
+    archivio,
+    "readonly",
+    store => store.get(id)
+  );
+}
+
+function leggiTutti(archivio) {
+  return eseguiRichiesta(
+    archivio,
+    "readonly",
+    store => store.getAll()
+  );
+}
+
+function eliminaDato(archivio, id) {
+  return eseguiRichiesta(
+    archivio,
+    "readwrite",
+    store => store.delete(id)
+  );
+}
+
+function svuotaArchivio(archivio) {
+  return eseguiRichiesta(
+    archivio,
+    "readwrite",
+    store => store.clear()
+  );
+}
+
+async function sostituisciArchivi(dati) {
   const db = await openDatabase();
 
   return new Promise((resolve, reject) => {
-    const transaction = db.transaction(archivio, "readonly");
-    const store = transaction.objectStore(archivio);
-    const request = store.getAll();
+    const transaction = db.transaction(ARCHIVI, "readwrite");
 
-    request.onsuccess = () => resolve(request.result);
-    request.onerror = () => reject(request.error);
+    transaction.oncomplete = () => resolve();
+    transaction.onerror = () => reject(transaction.error);
+    transaction.onabort = () => reject(transaction.error || new Error("Ripristino annullato"));
+
+    try {
+      ARCHIVI.forEach(nomeArchivio => {
+        const store = transaction.objectStore(nomeArchivio);
+        store.clear();
+
+        const elementi = Array.isArray(dati[nomeArchivio])
+          ? dati[nomeArchivio]
+          : [];
+
+        elementi.forEach(elemento => {
+          store.put(elemento);
+        });
+      });
+    } catch (error) {
+      transaction.abort();
+      reject(error);
+    }
   });
 }
 
 window.PrezziDB = {
+  DB_NAME,
+  DB_VERSION,
+  ARCHIVI: [...ARCHIVI],
   openDatabase,
   aggiungiDato,
-  leggiTutti
+  salvaDato,
+  leggiDato,
+  leggiTutti,
+  eliminaDato,
+  svuotaArchivio,
+  sostituisciArchivi
 };
